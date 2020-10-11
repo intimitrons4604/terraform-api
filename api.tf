ef687c98bba497edd2e5a4732a470dc839b8a1e8
pgfisico <pgfisico.dev@gmail.com>
2020-10-11 00:09:23 -0600

    Add default API Stage (#3)

--- api.tf
+++ api.tf
@@ -8,3 +8,15 @@ resource "aws_apigatewayv2_api" "gateway" {
     "trons:terraform"   = "true"
   }
 }
+
+resource "aws_apigatewayv2_stage" "gateway_default_stage" {
+  api_id      = aws_apigatewayv2_api.gateway.id
+  name        = "$default"
+  auto_deploy = true
+
+  tags = {
+    "trons:environment" = var.environment
+    "trons:service"     = "api"
+    "trons:terraform"   = "true"
+  }
+}
